701a4105090a4abea3a85fd05789530b23dbdacc
Eric Reed <mrericcoolnessreed@gmail.com>
2021-08-23 12:35:25 -0400

    removed repetitive docstrings from dummy fakessl

--- requests_ja3/imitate/fakessl/src/fakessl.c
+++ requests_ja3/imitate/fakessl/src/fakessl.c
@@ -10,7 +10,6 @@ static PyObject* pyincrement (PyObject* self, PyObject* input) {
     unsigned long output = increment (as_unsigned_long);
     return PyLong_FromUnsignedLong (output);
 }
-PyDoc_STRVAR(increment_doc, "Increments the input integer.");
 
 
 PyMethodDef fakessl_methods [] = {
@@ -18,16 +17,15 @@ PyMethodDef fakessl_methods [] = {
         .ml_name = "increment",
         .ml_meth = (PyCFunction) pyincrement,
         .ml_flags = METH_O,
-        .ml_doc = increment_doc
+        .ml_doc = NULL
     },
     {NULL}
 };
 
-PyDoc_STRVAR(fakessl_doc, "Drop-in replacement that implements a subset of Python's SSL module");
 PyModuleDef fakessl_module = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "fakessl",
-    .m_doc = fakessl_doc,
+    .m_doc = NULL,
     .m_size = -1,
     .m_methods = fakessl_methods,
     .m_slots = NULL,
